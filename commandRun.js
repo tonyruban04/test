5f354d7a1be1770d95b5aca351e08977b88bfebb
Tony Ruban Raj <tonyruban04@gmail.com>
2019-04-11 15:34:54 +0530

    created files

---
+++ commandRun.js
@@ -0,0 +1,11 @@
+var sys = require('sys')
+var exec = require('child_process').exec;
+var child;
+// executes `pwd`
+child = exec("pwd", function (error, stdout, stderr) {
+  sys.print('stdout: ' + stdout);
+  sys.print('stderr: ' + stderr);
+  if (error !== null) {
+    console.log('exec error: ' + error);
+  }
+});
