82c46510834ebf671fa0237686a434106ee3caf1
Tony Ruban Raj <tonyruban04@gmail.com>
2019-04-11 16:18:33 +0530

    commit

--- commandRun.js
+++ commandRun.js
@@ -1,6 +1,7 @@
 var sys = require('sys')
 var exec = require('child_process').exec;
 var child;
+var p;
 // executes `pwd`
 child = exec("pwd", function (error, stdout, stderr) {
   sys.print('stdout: ' + stdout);
